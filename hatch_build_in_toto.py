f3f22e9dd9ab595a4cf5e4c860d1c21805b82387
Lukas Puehringer <lukas.puehringer@nyu.edu>
2022-04-29 16:43:42 +0200

    Provide option to configure key via envvar (WIP)
    To use the plugin with CI and locally with different keys, we want
    to configure different signing keys for the plugin w/o pyproject.toml
    
    TODO:
    - excluding artifacts is a pain :/
      -> maybe it becomes easier when using base path instead of lstrip
      -> file in-toto issue (for better handling or better doc)
    
    - Figure out if there are better ways to configure the hatch build
      plugin w/o pyproject.toml

--- hatch_build_in_toto.py
+++ hatch_build_in_toto.py
@@ -10,28 +10,66 @@ artifact(s) as products, signing the resulting link metadata with a configured
 functionary key.
 
 """
+import json
+import os
+
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 from in_toto.models.link import FILENAME_FORMAT, Link
 from in_toto.models.metadata import Metablock
 from in_toto.runlib import record_artifacts_as_dict
 
 
+class EnvVars:
+    KEY = "HATCH_IN_TOTO_KEY"
+    GPG_KEYID = "HATCH_IN_TOTO_GPG_KEYID"
+
+
+# Define filters for recording materials in the project directory
+# NOTE: No filters needed for products, because they are recorded explicitly
+# NOTE: Leading wildcards for path fragments that seemingly start at root are needed,
+# because the actual root of the path is /. in-toto only strips the left part in the end.
+# paths
+MATERIAL_EXCLUDES = [
+    "dist",
+    "*/docs/build",
+    "*/tests/htmlcov",
+    ".*",
+    "*~",
+    "*.egg-info",
+    "*.link*",
+    "*.pyc",
+]
+
+
 class InTotoBuildHook(BuildHookInterface):
-    def _record_artifacts(self, path):
-        return record_artifacts_as_dict(
-            [path],
-            exclude_patterns=[".*", "*~", "*.link*", "*.pyc"],
-            normalize_line_endings=True,
-            lstrip_paths=[f"{self.root}/"],
-        )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Common config for artifact recording
+        self.lstrip = [f"{self.root}/"]
+
+        # Parse config from envvars
+        key_data = os.environ.get(EnvVars.KEY)
+        self.gpg_keyid = os.environ.get(EnvVars.GPG_KEYID)
+        if (key_data is not None) == (self.gpg_keyid is not None):
+            raise ValueError(
+                f"Exactly one of `{EnvVars.KEY} or {EnvVars.GPG_KEYID}` must be set"
+            )
+
+        if key_data:
+            self.key = json.loads(key_data)
 
     def initialize(self, version, build_data):
-        self.materials = self._record_artifacts(self.root)
+        self.materials = record_artifacts_as_dict(
+            [self.root],
+            exclude_patterns=MATERIAL_EXCLUDES,
+            lstrip_paths=self.lstrip,
+        )
 
     def finalize(self, version, build_data, artifact_path):
-        products = self._record_artifacts(artifact_path)
-
-        name = self.target_name
+        products = record_artifacts_as_dict(
+            [artifact_path], lstrip_paths=self.lstrip
+        )
 
         link = Metablock(
             signed=Link(
@@ -40,10 +78,15 @@ class InTotoBuildHook(BuildHookInterface):
                 products=products,
             )
         )
+        if self.key:
+            sig = link.sign(self.key)
+        elif self.gpg_keyid:
+            sig = link.sign_gpg(self.gpg_keyid)
+        else:
+            raise RuntimeError("No signing key")
 
-        sig = link.sign_gpg()
-        link.dump(
-            FILENAME_FORMAT.format(
-                step_name=self.target_name, keyid=sig["keyid"]
-            )
+        link_filename = FILENAME_FORMAT.format(
+            step_name=self.target_name, keyid=sig["keyid"]
         )
+
+        link.dump(os.path.join(self.directory, link_filename))
