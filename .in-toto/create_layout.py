9b081aa53797442e1c8a72a249f035c63d42a1fa
Lukas Puehringer <lukas.puehringer@nyu.edu>
2022-05-02 13:52:09 +0200

    Add layout creation script (wip)
    Add simple layout creation script to create layouts with exactly
    one build step, with treshold 2.
    
    This can be used to verify reproducible builds, each compare the
    products (wheel or sdist) from cd, with those locally created.
    
    TODO:
    - should this be checked into vcs?
    - write docs how to sign, verify, and what's happening
    - integrate verify with `verify_release`
    - create link for tests and add to layout with match rules
    - create untar (sdist) and unzip (wheel) inspections with match
      rules
    - add `sign_release` as cli for `export HATCH_... python build`
      -> move `verify_release --sign` to `sign_release`
      could have two ways of invocation
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

---
+++ .in-toto/create_layout.py
@@ -0,0 +1,44 @@
+from in_toto.models.layout import Inspection, Layout, Step
+from in_toto.models.metadata import Metablock
+from securesystemslib.gpg.functions import export_pubkeys
+
+cd_key = {
+    "keyid": "5c98490d78e6ac4401b065870b6f612587ff69dd89176099af95d1c4615afd8f",
+    "keytype": "ed25519",
+    "scheme": "ed25519",
+    "keyval": {
+        "public": "d685e2c2cfd1f2d79f5c301b25b3e0bc83f951c47d2d13a93d715fed721ca20d"
+    },
+}
+
+maintainer_keyids = [
+    "8ba69b87d43be294f23e812089a2ad3c07d962e8",
+]
+
+maintainer_keys = export_pubkeys(maintainer_keyids)
+
+
+for build in ["sdist", "wheel"]:
+
+    layout = Layout()
+    layout.set_relative_expiration(months=12)
+
+    for gpg_keyid in [cd_key] + list(maintainer_keys.values()):
+        layout.add_functionary_key(gpg_keyid)
+
+    build_step = Step(name=build)
+    build_step.pubkeys = [cd_key["keyid"]] + maintainer_keyids
+    build_step.threshold = 2
+
+    layout.steps = [build_step]
+
+    metablock = Metablock(signed=layout)
+    metablock.dump(f"{build}.layout")
+
+    # Sign layouts in CLI
+    # in-toto-sign -g 8ba69b87d43be294f23e812089a2ad3c07d962e8 -f sdist.layout
+    # in-toto-sign -g 8ba69b87d43be294f23e812089a2ad3c07d962e8 -f wheel.layout
+
+# Verify with sdist and wheel links in same dir
+# in-toto-verify --layout sdist.layout --gpg 8ba69b87d43be294f23e812089a2ad3c07d962e8
+# in-toto-verify --layout sdist.layout --gpg 8ba69b87d43be294f23e812089a2ad3c07d962e8
